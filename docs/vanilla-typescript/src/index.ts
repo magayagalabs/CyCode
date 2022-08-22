e944727f3d2c272c5b902ee11fed2c1cf6958774
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2022-08-22 19:31:17 +0800

    Create index.ts

---
+++ docs/vanilla-typescript/src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2022 Cyril John Magayaga
+ * Cyflex (based on CyCode)
+ */
+
+ import "./styles.css";
+
+ // Cyflex first paragraph and logo
+ document.getElementById("cyflex-first").innerHTML = `
+ <div class="cyflex-text">
+    <h1>Hello, Welcome to the CyCode!</h1>
+ </div>
+ <div class="cyflex-paragraph">
+    <p><b>CyCode</b> (also known as "<b>Cyril Code</b>" and formerly known as <b>Studios Code</b>) is a code editor for web development created and developed by <a href="https://github.com/Magayaga">Cyril John Magayaga</a>. It is the written in <b>HTML</b> standard markup language, <b>CSS</b> stylesheet languages, and <b>JavaScript</b> programming language.</p> <p>It is the first <b>family of the edit editors</b> in the <b>Philippines</b> (or <b>about this world</b>), like source-code editors, code editors, text editors, IDE or integrated development environments, document editors, rich text editors, WYSIWYG editors, and more. <b>CyCode</b> was released on December 23, 2021 in Quezon City, Metro Manila, Philippines by Cyril John Magayaga.</p>
+    <p><b>Weaver</b> is a best known for the Cyril John Magayaga's creating <b>first text editor</b>, after discontinued text editor and failed building web application. It is the history of the <b>Weaver & CyCode</b>:</p>
+ </div>
+ <div class="cyflex-paragraph">
+    <p><b>CyCode Family</b> is a all text editors written in <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>TypeScript</b>, <b>Java</b>, <b>Python</b>, <b>Rust</b>, <b>Less</b>, <b>Scss</b>, <b>CoffeeScript</b>, <b>C#</b>, <b>Visual Basic .NET</b>, <b>C++</b>, and more programming languages.</p>
+ </div>
+ `;
+ 
+ // Cyflex second paragraph & pictures
+ document.getElementById("cyflex-second").innerHTML = `
+ <div class="cyflex-paragraph">
+    <p><b><i>Upcoming products: </i></b><b>CyCode Studio</b> & <b>CyWrite</b> in September 5, 2022, <b>CyCode for Python</b> & <b>CyCode for Node.js</b> in October 5, 2022, <b>CyCharm</b> in December 22, 2022, and <b>CyRider</b> in December 10, 2025.</p>
+    <p><b><i>Current products: </i></b><b>Weaver</b> (Discontinued), and <b>CyCode</b> (Active)</p>
+ </div>
+ `;
+

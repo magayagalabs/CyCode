7a4afd16a10b4da9634ca5a5b0aa19efac9018ad
cyrilmagayaga <cyrilmagayaga@gmail.com>
2023-12-19 16:37:48 +0800

    Update release (v1.3.1): Fixing and Updating HTML and JavaScript files

---
+++ src/js/link.js
@@ -0,0 +1,14 @@
+function GitHubCyCode() {
+    var url = "https://github.com/magayagalabs/CyCode";
+    window.open(url, "_blank");
+}
+
+function LicenseMIT() {
+    var url = "https://opensource.org/license/mit/";
+    window.open(url, "_blank");
+}
+
+function LicenseCyCode() {
+    var url = "https://github.com/Magayaga/CyCode/blob/main/LICENSE";
+    window.open(url, "_blank");
+}

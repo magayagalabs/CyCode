5efad46b85157be600036b1a43bbe23960366d6f
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-01-30 11:44:27 +0800

    Update release (v2.0.3): Updating console-md.ts

--- src/ts/console-md.ts
+++ src/ts/console-md.ts
@@ -1,3 +1,4 @@
-console.log("CyCode - HTML & Markdown Editor\n");
-console.log("Copyright 2021-2023 Cyril John Magayaga\n\n");
+console.log("CyCode - HTML, Markdown, and WYSIWYG Editor\n");
+console.log("Copyright 2021-2024 Cyril John Magayaga\n\n");
 console.log("Markdown is the lightweight markup language.");
+console.log("HTML is the standard markup language.");
